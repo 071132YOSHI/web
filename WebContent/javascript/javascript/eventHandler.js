5a63d4a6178dcb9fc50ac5b8117d7e7ba86162ff
Yoshida-Yusuke <yusuke071132543@yahoo.co.jp>
2017-08-01 01:22:59 +0900

    12

---
+++ WebContent/javascript/javascript/eventHandler.js
@@ -0,0 +1,19 @@
+//jsの標準機能の本気を出す。
+//まずはイベントハンドラー制覇
+
+//1、とりあえずonclick,onDblClick(ダブルクリック)
+
+function onclick(){
+	
+	alert("onclick");
+	
+	
+	
+}
+
+
+function onDblClick(){
+	
+	alert("onDblClick");
+	
+}
